115e822b623c7e3fe9c3ebda2b8775b918be6b87
Steven Lee <gyl_mail@qq.com>
2019-07-04 16:00:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-trainpath = r"D:\Users\yl_gong\Desktop\dl\voc\VOCtest_06-Nov-2007\VOCdevkit\VOC2007\JPEGImages"
-styleimg = r'D:\Users\yl_gong\Desktop\rain_princess.jpg'
-modelpath = r"D:\Users\yl_gong\Desktop\ckp\ckp"
-testimg = r"D:\Users\yl_gong\Desktop\chicago.jpg"
-testpath = r"D:\Users\yl_gong\Desktop\tp"
-savepath = r"D:\Users\yl_gong\Desktop\sp"
+trainpath = r"D:\Users\xxx\Desktop\dl\voc\VOCtest_06-Nov-2007\VOCdevkit\VOC2007\JPEGImages"
+styleimg = r'D:\Users\xxx\Desktop\rain_princess.jpg'
+modelpath = r"D:\Users\xxx\Desktop\ckp\ckp"
+testimg = r"D:\Users\xxx\Desktop\chicago.jpg"
+testpath = r"D:\Users\xxx\Desktop\tp"
+savepath = r"D:\Users\xxx\Desktop\sp"
 epoch = 2
 batchsize = 4
 
